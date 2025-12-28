bf9881d6d3b47ee529be87486476346803ef5054
Yash777Salave <yashsalave777@rediffmail.com>
2025-12-28 14:27:52 +0530

    image carousal done with auto slide functionality

--- src/components/Carousel/ImageSlider.jsx
+++ src/components/Carousel/ImageSlider.jsx
@@ -1,34 +1,70 @@
-import {
-  Dimensions,
-  FlatList,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import React from 'react';
+import { Dimensions, FlatList, Image, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import { sliderImages } from '../../utils/sliderImeges';
 import { styles } from './style';
+
 const ImageSlider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const width = Dimensions.get('screen').width;
-  const renderItem = ({ item }) => {
-    return (
-      <View>
-        <Image source={item.image} style={styles.images} />
-      </View>
-    );
+  const flatListRef = useRef(null);
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Image source={item.image} style={styles.images} />
+    </View>
+  );
+
+  const renderDotIndicators = () =>
+    sliderImages.map((_, index) => (
+      <View
+        key={index}
+        style={index === activeIndex ? styles.activeDots : styles.dots}
+      />
+    ));
+
+  const handleOnScroll = event => {
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(scrollPosition / width);
+    setActiveIndex(currentIndex);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!flatListRef.current) return;
+
+      const nextIndex =
+        activeIndex === sliderImages.length - 1 ? 0 : activeIndex + 1;
+
+      flatListRef.current.scrollToIndex({
+        index: nextIndex,
+        animated: true,
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
+  const getItemLayout = (_, index) => ({
+    length: width,
+    offset: width * index,
+    index,
+  });
+
   return (
     <View>
       <FlatList
+        ref={flatListRef}
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         data={sliderImages}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
-        style={{ marginTop: 10 }}
+        keyExtractor={item => item.id.toString()}
+        onScroll={handleOnScroll}
+        getItemLayout={getItemLayout}
       />
+
+      <View style={styles.dotsContainer}>{renderDotIndicators()}</View>
     </View>
   );
 };
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -7,4 +7,30 @@ export const styles = StyleSheet.create({
     height: 195,
     width: width,
   },
+  dots: {
+    backgroundColor: CONSTANTS.THEME.PRIMARY_COLOR,
+
+    height: 9,
+    width: 9,
+    borderRadius: 10,
+    // margin: 5,
+    marginHorizontal: 6,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  activeDots: {
+    backgroundColor: CONSTANTS.THEME.SECONDARY_COLOR,
+    height: 9,
+    width: 9,
+    borderRadius: 10,
+    // margin: 5,
+    marginHorizontal: 6,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
