8d9f25c949d43f6ce4e9136b0478d869823c735a
Yash777Salave <yashsalave777@rediffmail.com>
2026-01-07 22:10:06 +0530

    working on Orders Screen's Ongoing card's UI

---
+++ src/components/OrdersCard/OrdersCard.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const OrdersCard = ({}) => {
+  return (
+    <View>
+      <Text>OrdersCard</Text>
+    </View>
+  );
+};
+
+export default OrdersCard;
--- src/screens/Orders/OngoingScreen/index.jsx
+++ src/screens/Orders/OngoingScreen/index.jsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import { OngoingList } from '../../../utils/OrderCardList';
 const OngoingScreen = () => {
+  const renderItem = ({ item }) => (
+    <View key={item.id}>
+      <Text>{item.id}</Text>
+    </View>
+  );
   return (
     <View>
       <Text>OngoingScreen</Text>
+      <FlatList
+        data={OngoingList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 };
